5e8f9b8de2fdba3ce2f882fe215265b15fd6c49a
Gayathri <gayathricg4050.com>
2024-08-30 15:04:44 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=Ramesh
+person2=Raju
+
+echo ${person1}:"Hello ${person2}, How are you"
+echo ${person2}:"I am good Raju, What about you"
+echo ${person1}:" iam practicing shell script ${person2}"
